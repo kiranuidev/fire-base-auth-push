d479e0fb0beaf2cdca979f09dc88737004b32a77
kiranpvs <kiran>
2019-04-19 13:00:19 +0530

    added project

---
+++ project/app/enviroments/firebase.js
@@ -0,0 +1,26 @@
+import firebase from '@firebase/app';
+import '@firebase/auth';
+
+const config ={
+  apiKey: "AIzaSyCcLHA5FYcCJeM21rt3r4yutOGs8ybxdfw",
+  authDomain: "fir-demo-e9197.firebaseapp.com",
+  databaseURL: "https://fir-demo-e9197.firebaseio.com",
+  projectId: "fir-demo-e9197",
+  storageBucket: "fir-demo-e9197.appspot.com",
+  messagingSenderId: "305839940027"
+};
+
+let instance = null;
+
+class FirebaseService {
+  constructor() {
+    if (!instance) {
+      this.app = firebase.initializeApp(config);
+      instance = this;
+    }
+    return instance;
+  }
+}
+
+const firebaseService = new FirebaseService().app;
+export default firebaseService;
